a54cd573ea07dc8e82fa7197c0629df569c07a29
c-barron <christinehbarron@gmail.com>
2024-05-30 16:40:09 -0700

    Working with owlv2 model from hf

--- predict.py
+++ predict.py
@@ -3,53 +3,72 @@ import sys
 import subprocess
 import torch
 
+from transformers import AutoModelForMaskGeneration, AutoProcessor, pipeline
+from PIL import Image
+
 #install GroundingDINO and segment_anything
 os.environ['CUDA_HOME'] = '/usr/local/cuda-11.7'
 os.environ['AM_I_DOCKER'] = 'true'
 os.environ['BUILD_WITH_CUDA'] = 'true'
 
-env_vars = os.environ.copy()
-HOME = os.getcwd()
-sys.path.insert(0, "weights")
-sys.path.insert(0, "weights/GroundingDINO")
-sys.path.insert(0, "weights/segment-anything")
-os.chdir("/src/weights/GroundingDINO")
-subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
-os.chdir("/src/weights/segment-anything")
-subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
-os.chdir(HOME)
+# env_vars = os.environ.copy()
+# HOME = os.getcwd()
+# sys.path.insert(0, "weights")
+# sys.path.insert(0, "weights/GroundingDINO")
+# sys.path.insert(0, "weights/segment-anything")
+# os.chdir("/src/weights/GroundingDINO")
+# subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
+# os.chdir("/src/weights/segment-anything")
+# subprocess.call([sys.executable, '-m', 'pip', 'install', '-e', '.'], env=env_vars)
+# os.chdir(HOME)
 
 from cog import BasePredictor, Input, Path, BaseModel
-from typing import Iterator
-from groundingdino.util.slconfig import SLConfig
-from groundingdino.models import build_model
-from groundingdino.util.utils import clean_state_dict
-from segment_anything import build_sam, SamPredictor
+from typing import Iterator, List, Optional
+# from groundingdino.util.slconfig import SLConfig
+# from groundingdino.models import build_model
+# from groundingdino.util.utils import clean_state_dict
+# from segment_anything import build_sam, SamPredictor
 from grounded_sam import run_grounding_sam
 import uuid
-from hf_path_exports import cache_config_file, cache_file
+# from hf_path_exports import cache_config_file, cache_file
+
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         print("Loading pipelines...x")
 
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        def load_detector(detector_id: Optional[str] = None):
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            detector_id = detector_id if detector_id is not None else "IDEA-Research/grounding-dino-tiny"
+            object_detector = pipeline(model=detector_id, task="zero-shot-object-detection", device=device)
+            return object_detector
+        
+        
+        def load_segmentator(segmenter_id: Optional[str] = None):
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            segmenter_id = segmenter_id if segmenter_id is not None else "facebook/sam-vit-base"
 
-        def load_model_hf(device='cpu'):
-            args = SLConfig.fromfile(cache_config_file)
-            args.device = device
-            model = build_model(args)
-            checkpoint = torch.load(cache_file, map_location=device)
-            log = model.load_state_dict(clean_state_dict(checkpoint['model']), strict=False)
-            print("Model loaded from {} \n => {}".format(cache_file, log))
-            _ = model.eval()
-            return model
+            segmentator = AutoModelForMaskGeneration.from_pretrained(segmenter_id).to(device)
+            processor = AutoProcessor.from_pretrained(segmenter_id)
+            return segmentator, processor
 
-        self.groundingdino_model = load_model_hf(device)
-        sam_checkpoint = '/src/weights/sam_vit_h_4b8939.pth'
-        self.sam_predictor = SamPredictor(build_sam(checkpoint=sam_checkpoint).to(device))
+        detector_id = "google/owlv2-base-patch16-ensemble"
+        segmenter_id = "facebook/sam-vit-base"
+        
+        object_detector = load_detector(detector_id)
+        segmentator, processor = load_segmentator(segmenter_id)
 
+        self.object_detector = object_detector
+        self.segmentator = segmentator
+        self.processor = processor
+
+        
+        print("Pipelines loaded...x")
+    
+
+
+    
     @torch.inference_mode()
     def predict(
             self,
@@ -57,44 +76,42 @@ class Predictor(BasePredictor):
                 description="Image",
                 default="https://st.mngbcn.com/rcs/pics/static/T5/fotos/outfit/S20/57034757_56-99999999_01.jpg",
             ),
-            mask_prompt: str = Input(
-                description="Positive mask prompt",
-                default="clothes,shoes",
+            prompts: List[str] = Input(
+                description="List of mask prompts. Each should end with a period?",
+                default=["face."],
             ),
-            negative_mask_prompt: str = Input(
-                description="Negative mask prompt",
-                default="pants",
-            ),
-            adjustment_factor: int = Input(
-                description="Mask Adjustment Factor (-ve for erosion, +ve for dilation)",
-                default=0,
-            ),
+            threshold: float = Input(
+                description="Cutof for object detection",
+                default=0.15,
+            )
     ) -> Iterator[Path]:
         """Run a single prediction on the model"""
         predict_id = str(uuid.uuid4())
 
         print(f"Running prediction: {predict_id}...")
 
-        annotated_picture_mask, neg_annotated_picture_mask, mask, inverted_mask = run_grounding_sam(image,
-                                                                                                    mask_prompt,
-                                                                                                    negative_mask_prompt,
-                                                                                                    self.groundingdino_model,
-                                                                                                    self.sam_predictor,
-                                                                                                    adjustment_factor)
+        outputs = run_grounding_sam(image, prompts, self.object_detector, self.segmentator, self.processor, threshold)
+
         print("Done!")
+        print(outputs)
 
-        variable_dict = {
-            'annotated_picture_mask': annotated_picture_mask,
-            'neg_annotated_picture_mask': neg_annotated_picture_mask,
-            'mask': mask,
-            'inverted_mask': inverted_mask
-        }
 
+        # Create a list to store the output paths
+        output_paths = []
+    
         output_dir = "/tmp/" + predict_id
-        os.makedirs(output_dir, exist_ok=True)  # create directory if it doesn't exist
+        os.makedirs(output_dir, exist_ok=True)
+    
+        # Create a black image for fallback
+        fallback_image = Image.new('RGB', (10, 10), color='black')
 
-        for var_name, img in variable_dict.items():
-            random_filename = output_dir + "/" + var_name + ".jpg"
-            rgb_img = img.convert('RGB')  # Converting image to RGB
-            rgb_img.save(random_filename)
-            yield Path(random_filename)
+        # Iterate over the prompts and yield the corresponding mask or fallback image
+        for prompt in prompts:
+            print("Prompt: ", prompt)
+            image = outputs.get(prompt, fallback_image)
+            random_filename = os.path.join(output_dir, f"{prompt.replace(' ', '_')}.jpg")
+            if image.mode != 'RGB':
+                print("Converting image to RGB")
+                image = image.convert('RGB')
+            image.save(random_filename)
+            yield Path(random_filename)  # Yield the path to the saved image
